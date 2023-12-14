cafa6e95b128f9047e44d0c92a7b3af7acdf8cd6
Andrewx07 <147568853+Andrewx07@users.noreply.github.com>
2023-12-13 22:39:15 -0400

    first commit

---
+++ Custom_models.py
@@ -0,0 +1,140 @@
+from torch import nn as nn
+
+
+# model
+class REDCN1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu_conv = nn.Sequential(
+            nn.Conv2d(3, 20, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(20, 30, 5),
+            nn.ReLU(),
+            nn.Conv2d(30, 40, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(1),
+        )
+
+        self.relu_linear = nn.Sequential(
+            nn.Linear(360, 700),
+            nn.SELU(),
+            nn.Linear(700, 300),
+            nn.SELU(),
+            # nn.Dropout(p=0.25),
+            nn.Linear(300, 150),
+            nn.SELU(),
+            nn.Linear(150, 80),
+            nn.SELU(),
+            nn.Linear(80, 100),
+        )
+
+    def forward(self, x):
+        conv_layers = self.relu_conv(x)
+        linear_layers = self.relu_linear(conv_layers)
+        return linear_layers
+
+
+# VGG
+class vgg16(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu_conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(1),
+        )
+
+        self.relu_linear = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 100),
+        )
+
+    def forward(self, x):
+        conv_layers = self.relu_conv(x)
+        linear_layers = self.relu_linear(conv_layers)
+        return linear_layers
+
+
+def conv(channels, out_channels, pool=False):
+    layers = [
+        nn.Conv2d(channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+    ]
+    if pool:
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+class ResNet9(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = conv(3, 150)
+        self.conv2 = conv(150, 300, pool=True)
+        self.res1 = nn.Sequential(conv(250, 250), conv(250, 250))
+
+        self.conv3 = conv(300, 500, pool=True)
+        self.conv4 = conv(500, 1000, pool=True)
+        self.res2 = nn.Sequential(conv(1000, 1000), conv(1000, 1000))
+
+        self.classifier = nn.Sequential(
+            nn.MaxPool2d(4), nn.Flatten(), nn.Dropout(p=0.2), nn.Linear(1000, 100)
+        )
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
---
+++ Training_Func.py
@@ -0,0 +1,87 @@
+import torch
+import numpy as np
+
+
+def training_cycle(model, training_time, optimizer, criterion, dataset):
+    graphic1 = []
+    graphic2 = []
+    running_epoch = []
+    accuracy = []
+    acc_val = []
+
+    for epoch in range(training_time):
+        train_loss = 0.0
+        valid_loss = 0.0
+
+        for inputs, labels in dataset["train_dataloader"]:
+            if torch.cuda.is_available():
+                inputs, labels = inputs.cuda(), labels.cuda()
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+
+        model.eval()
+        for inputs, labels in dataset["validation_dataloader"]:
+            if torch.cuda.is_available():
+                inputs, labels = inputs.cuda(), labels.cuda()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            valid_loss += loss.item()
+
+        running_epoch.append(float(epoch))
+
+        accuracy, class_correct_test, class_total_test, test_loss = testing_func(
+            dataset, model, criterion
+        )
+
+        train_loss = train_loss / len(dataset["train_dataloader"])
+        print("\n[Para la epoca", epoch + 1, "] \nloss:", train_loss)
+        graphic1.append(train_loss)
+
+        valid_loss = valid_loss / len(dataset["validation_dataloader"])
+        print("\nval_loss:", valid_loss)
+        graphic2.append(valid_loss)
+
+        print(f"\nTest Loss: {test_loss}")
+
+        print(
+            f"\nTest Accuracy : {float(100. * np.sum(class_correct_test) / np.sum(class_total_test))}% where {int(np.sum(class_correct_test))} of {int(np.sum(class_total_test))} were pmodelicted correctly"
+        )
+
+    return (
+        graphic1,
+        graphic2,
+        running_epoch,
+        accuracy,
+        class_total_test,
+        class_correct_test,
+        test_loss,
+    )
+
+
+def testing_func(dataset, model, criterion):
+    class_correct_test = list(0.0 for i in range(100))
+    class_total_test = list(0.0 for i in range(100))
+    test_loss = 0.0
+    accuracy = []
+    for images, lab in dataset["test_dataloader"]:
+        if torch.cuda.is_available():
+            images, lab = images.cuda(), lab.cuda()
+        output = model(images)
+        loss = criterion(output, lab)
+        test_loss += loss.item()
+        _, pmodel = torch.max(output, 1)
+        correct = np.squeeze(pmodel.eq(lab.data.view_as(pmodel)))
+
+        for idx in range(dataset["batch_size"]):
+            label = lab[idx]
+            class_correct_test[label] += correct[idx].item()
+            class_total_test[label] += 1
+
+        test_loss = test_loss / len(dataset["test_dataloader"])
+
+        accuracy.append(float(np.sum(class_correct_test) / np.sum(class_total_test)))
+    return accuracy, class_correct_test, class_total_test, test_loss
